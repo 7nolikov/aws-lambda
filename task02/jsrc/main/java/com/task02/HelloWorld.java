00a493014be9c4a8c5455dd888ba3a7f49822a57
Dmitrii_Novikov1 <dmitrii_novikov1@epam.com>
2024-05-02 16:20:58 +0200

    Added task02

---
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -0,0 +1,38 @@
+package com.task02;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
+
+@LambdaHandler(
+    lambdaName = "hello_world",
+    roleName = "hello_world-role",
+    isPublishVersion = true,
+    logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED)
+@LambdaUrlConfig(authType = AuthType.NONE, invokeMode = InvokeMode.BUFFERED)
+public class HelloWorld
+    implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+  public APIGatewayProxyResponseEvent handleRequest(
+      APIGatewayProxyRequestEvent request, Context context) {
+
+    context.getLogger().log(request.toString());
+
+    if (request.getPath().equals("/hello")) {
+      return new APIGatewayProxyResponseEvent().withBody("Hello from Lambda").withStatusCode(200);
+    } else {
+      return new APIGatewayProxyResponseEvent()
+          .withBody(
+              String.format(
+                  "Bad request syntax or unsupported method. Request path: %s. HTTP method: %s",
+                  request.getPath(), request.getHttpMethod()))
+          .withStatusCode(400);
+    }
+  }
+}
